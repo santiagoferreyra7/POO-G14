70d70c8aec0f624c322a508ca41981d563ed3fe6
santiagoferreyra7 <144260526+santiagoferreyra7@users.noreply.github.com>
2024-09-26 08:38:58 -0300

    Add files via upload

---
+++ papel.py
@@ -0,0 +1,19 @@
+# Clase para jugar contra la compu
+class JuegoConCompu(Juego):
+    def __init__(self):
+        super().__init__()
+
+    def jugar(self):
+        self.mostrar_reglas()
+        try:
+            movimiento_jugador = input("Elige: Piedra, Papel o Tijera: ").capitalize()
+            if movimiento_jugador not in self.movimientos:
+                raise ValueError("No se puede hacer eso")
+            
+            movimiento_compu = random.choice(self.movimientos)
+            print(f"La compu elige: {movimiento_compu}")
+            
+            resultado = self.determinar_ganador(movimiento_jugador, movimiento_compu)
+            print(resultado)
+        except ValueError as e:
+            print(e)
